24bb9f9fe3ea7bbe3a4fcff4b5f32f509d07da28
BartMoura <bart.emf@hotmail.com>
2021-03-31 14:46:30 -0300

    usando get e post

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
-// console.log(app);
+app.use(express.json()); // manda o json para o body da requisição
 
-app.get('/', (request, response) => {
-    response.send('Olá mundo!!!');
+const projects = [];
+
+app.get('/projects', (request, response) => {
+    const { title, owner } = request.query;
+    
+    // console.log(title);
+    // console.log(owner);
+
+    return response.json(projects);
 });
 
-app.listen(3000, () => {
-    console.log('Servidor rodando!');
+app.post('/projects', (request, response) =>{
+    const { title, owner } = request.body;
+    
+    // console.log(title);
+    // console.log(owner);
+
+    const project = {id: uuidv4(), title, owner};
+
+    projects.push(project); // esse push vai jogar a criação do nosso projeto para o nosso array
+
+    return response.json(project); // sempre retornar o projeto recém criado e nunca exibir a lista completa
 });
+
+app.put('/projects/:id', (request, response) => {
+    const params = request.params;
+    console.log(params);
+
+    return response.json([
+        'Projeto 50',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4',
+        'Projeto 5'
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 50',
+        'Projeto 2',
+    ]);
+});
+
+app.listen(3333);
